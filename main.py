096db64179e156c363aae3d337f50df19ed1592d
abubikdev <a.bubik@icloud.com>
2026-01-28 11:52:50 +0100

    Added basic menu of 5 functions

---
+++ main.py
@@ -0,0 +1,9 @@
+def selectAndUseFunction():
+    print("1. Obsah čtverce")
+    print("2. Obsah obdélníku")
+    print("3. Obsah koule")
+    print("4. Obsah krychle")
+    print("5. Obsah kvádru")
+
+    selectedFunction = input("> ")
+
