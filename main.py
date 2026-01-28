62c84e22c5ef665da1ade783d7adeb66312be178
abubikdev <a.bubik@icloud.com>
2026-01-28 11:56:40 +0100

    Created the first function

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-def selectAndUseFunction():
+def select_and_use_function():
     print("1. Obsah čtverce")
     print("2. Obsah obdélníku")
     print("3. Obsah koule")
     print("4. Obsah krychle")
     print("5. Obsah kvádru")
 
-    selectedFunction = input("> ")
+    selected_function = input("> ")
 
+    if selected_function == "1":
+        side_a: str = input("Zadejte délku jedné strany > ")
+
+        result = int(side_a) * int(side_a)
+        print("Obsah čtverce je" + result)
+        return
+
+
+
+
+
+select_and_use_function()
